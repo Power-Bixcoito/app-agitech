41aa4fc1b8389f6d0f3a37b0f13bd112ef4becd6
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-08-27 03:57:28 -0300

    fazendo parte da tela

--- src/pages/teen/home-teen/home-teen.ts
+++ src/pages/teen/home-teen/home-teen.ts
@@ -1,5 +1,7 @@
+import { SonsProvider } from './../../../providers/sons/sons';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { get_data } from '../../../utils/localStorage'
 
 @IonicPage()
 @Component({
@@ -7,12 +9,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home-teen.html',
 })
 export class HomeTeenPage {
+  user = get_data('user');
+  tasks;
+  activities;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private sons: SonsProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomeTeenPage');
+    this.getTasks();
+    this.getActivities();
+  }
+
+  getTasks() {
+    this.sons.getTasks().subscribe(tasks => this.tasks = tasks)
+  }
+
+  getActivities() {
+    this.sons.getActivities().subscribe(activities => this.activities = activities)
   }
 
 }
